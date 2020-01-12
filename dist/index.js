917a9edbe7ac5b7e3acfe84ac240027f91bc0769
tterian <tigran.terian@gmail.com>
2020-01-12 19:03:27 +0400

    add fauna setup

--- dist/index.js
+++ dist/index.js
@@ -9072,12 +9072,12 @@ const exec = __webpack_require__(653)
 const github = __webpack_require__(545)
 const io = __webpack_require__(707)
 
-const install = async() => {
+const install = async () => {
   console.log("installing NPM dependencies using Yarn")
   await exec.exec("yarn install")
 }
 
-const start = async() => {
+const start = async () => {
   console.log("starting lambda server")
   await exec.exec("yarn ci:lambda")
 
@@ -9088,16 +9088,24 @@ const start = async() => {
   await exec.exec("yarn ci:wait")
 }
 
-const test = async() => {
+const test = async () => {
   console.log(process.env.CYPRESS_RECORD_KEY)
   console.log("starting Cypress tests")
   await exec.exec("npx cypress run --record --browser chrome")
+}
+
+const faunaSetup = async () => {
+  console.log("setting up Fauna")
+  await exec.exec(`./node_modules/.bin/fauna eval talenth-ci --file=./fauna/cleanup.fql --secret=${process.env.PARENT_FAUNADB_KEY}`)
+  await exec.exec(`./node_modules/.bin/fauna eval talenth-ci --file=./fauna/seed/recruiter.fql --secret=${process.env.PARENT_FAUNADB_KEY}`)
+  await exec.exec(`./node_modules/.bin/fauna eval talenth-ci --file=./fauna/seed/applicant.fql --secret=${process.env.PARENT_FAUNADB_KEY}`)
 }
 
 async function run() {
   try {
     await install()
     await start()
+    await faunaSetup()
     await new Promise(resolve => setTimeout(resolve, 1000))
     await test()
 
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const exec = require('@actions/exec')
 const github = require('@actions/github')
 const io = require('@actions/io')
 
-const install = async() => {
+const install = async () => {
   console.log("installing NPM dependencies using Yarn")
   await exec.exec("yarn install")
 }
 
-const start = async() => {
+const start = async () => {
   console.log("starting lambda server")
   await exec.exec("yarn ci:lambda")
 
@@ -19,16 +19,24 @@ const start = async() => {
   await exec.exec("yarn ci:wait")
 }
 
-const test = async() => {
+const test = async () => {
   console.log(process.env.CYPRESS_RECORD_KEY)
   console.log("starting Cypress tests")
   await exec.exec("npx cypress run --record --browser chrome")
+}
+
+const faunaSetup = async () => {
+  console.log("setting up Fauna")
+  await exec.exec(`./node_modules/.bin/fauna eval talenth-ci --file=./fauna/cleanup.fql --secret=${process.env.PARENT_FAUNADB_KEY}`)
+  await exec.exec(`./node_modules/.bin/fauna eval talenth-ci --file=./fauna/seed/recruiter.fql --secret=${process.env.PARENT_FAUNADB_KEY}`)
+  await exec.exec(`./node_modules/.bin/fauna eval talenth-ci --file=./fauna/seed/applicant.fql --secret=${process.env.PARENT_FAUNADB_KEY}`)
 }
 
 async function run() {
   try {
     await install()
     await start()
+    await faunaSetup()
     await new Promise(resolve => setTimeout(resolve, 1000))
     await test()
 
